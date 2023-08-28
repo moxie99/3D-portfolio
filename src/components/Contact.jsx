854338e9a91117d0f88a90e978a6f22f1a3c2830
Adeolu David <adeolusegun99@gmail.com>
2023-08-28 03:11:38 +0100

    changed node version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'David',
+          to_name: 'David Moxie',
           from_email: form.email,
           to_email: 'adeolusegun99@gmail.com',
           message: form.message,
