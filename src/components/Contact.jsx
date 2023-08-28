9ec1a6e29db912f624fe0676da378a210121f065
Adeolu David <adeolusegun99@gmail.com>
2023-08-28 03:05:23 +0100

    added .env foor emailjs details in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,6 @@ const Contact = () => {
     email: '',
     message: '',
   });
-
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
@@ -37,9 +36,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'JavaScript Mastery',
+          to_name: 'David',
           from_email: form.email,
-          to_email: 'sujata@jsmastery.pro',
+          to_email: 'adeolusegun99@gmail.com',
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
